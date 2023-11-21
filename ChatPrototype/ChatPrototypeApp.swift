1f6d652d03860a91ce038173757ccc4bbdb92e09
Denis Chernyatin <ya.chernyatin@gmail.com>
2023-11-21 14:51:16 +0300

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Денис Чернятин on 2023.11.21.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
